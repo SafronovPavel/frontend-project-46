6c851f0f0acff5ed69fe225bdd432dbf85d24c49
PAVEL SAFRONOV <908916@gmail.com>
2023-08-19 18:57:39 +0700

    add test

---
+++ __tests__/index.test.js
@@ -0,0 +1,5 @@
+import genDiff from '../index.js';
+
+test('gendiff', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual('- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true');
+});
--- index.js
+++ index.js
@@ -1,51 +1,35 @@
 #!/usr/bin/env node
-
-import _ from "lodash";
 import fs from 'fs';
+// import _ from "lodash";
 
-
-
-
-// const getParsedDate = (file, ext) => {
-//   switch(ext) {
-//     case '.json': return JSON.parse(file)
-//   }
-// }
-
-const genDiff = (obj1, obj2) => {
-
-
-
-  obj1 = fs.readFileSync(`./${obj1}`, 'utf8');
-  obj2 = fs.readFileSync(`./${obj2}`, 'utf8');
-  
-  obj1 = JSON.parse(obj1)
-  obj2 = JSON.parse(obj2)
+const genDiff = (firstFile, secondFile) => {
+  const readFirstFile = fs.readFileSync(`./${firstFile}`, 'utf8');
+  const readSecondFile = fs.readFileSync(`./${secondFile}`, 'utf8');
+  const obj1 = JSON.parse(readFirstFile);
+  const obj2 = JSON.parse(readSecondFile);
   const result = [];
 
-  const keys = Object.keys({...obj1, ...obj2}).sort()
-
-  
+  const keys = Object.keys({ ...obj1, ...obj2 }).sort();
 
   for (const key of keys) {
     if (!Object.hasOwn(obj1, key)) {
-      result.push({key: key, value: obj2[key], status:'added'})
+      result.push({ key: key, value: obj2[key], status: 'added' });
     } else if (!Object.hasOwn(obj2, key)) {
-      result.push({key: key, value: obj1[key], status:'deleted'})
+      result.push({ key: key, value: obj1[key], status: 'deleted' });
     } else if (obj1[key] !== obj2[key]) {
-      result.push({key: key, value: obj1[key], status:'was'});
-      result.push({key: key, value: obj2[key], status:'be'});
+      result.push({ key: key, value: obj1[key], status: 'was' });
+      result.push({ key: key, value: obj2[key], status: 'be' });
     } else if (obj1[key] === obj2[key]) {
-      result.push({key: key, value: obj1[key], status:'unchanged'});
+      result.push({ key: key, value: obj1[key], status: 'unchanged' });
     }
   }
 
-    return result.map((pair) => {
-      if (pair.status === 'added') return `+ ${pair.key}: ${pair.value}`
-      if (pair.status === 'deleted') return `- ${pair.key}: ${pair.value}`
-      if (pair.status === 'was') return `- ${pair.key}: ${pair.value}`
-      if (pair.status === 'be') return `+ ${pair.key}: ${pair.value}`
-      if (pair.status === 'unchanged') return `  ${pair.key}: ${pair.value}`}).join(' \n')
+  return result.map((pair) => {
+    if (pair.status === 'added') return `+ ${pair.key}: ${pair.value}`;
+    if (pair.status === 'deleted') return `- ${pair.key}: ${pair.value}`;
+    if (pair.status === 'was') return `- ${pair.key}: ${pair.value}`;
+    if (pair.status === 'be') return `+ ${pair.key}: ${pair.value}`;
+    if (pair.status === 'unchanged') return `  ${pair.key}: ${pair.value}`;
+  }).join(' \n');
 };
-
 export default genDiff;
